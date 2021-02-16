0e7cd4464141a352367df69167405feb650ed6dd
Bernard Kolobara <me@kolobara.com>
2021-02-16 10:08:25 +0100

    Add channel test

--- test/channels.ts
+++ test/channels.ts
@@ -1,1 +1,23 @@
-// TODO: validate the assembly functions work by passing around some messages between channels
+import { Console } from "as-wasi";
+import {Channel} from "../assembly/lunatic";
+
+export function _start(): void {
+    let c = Channel.create(0);
+    let message = StaticArray.fromArray<u8>([137,42]);
+    c.send(message);
+    let result  = c.receive();
+    if (result != null) {
+        Console.log("Received: " + result.toString());
+    }
+    
+}
+
+function abort(
+    message: string | null,
+    fileName: string | null,
+    lineNumber: u32,
+    columnNumber: u32
+): void
+{
+    // Nothing to do
+}
