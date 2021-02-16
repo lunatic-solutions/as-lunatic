11fa501b50d093e422e5a3908b1b4e7a2d8998d0
Joshua Tenner <tenner.joshua@gmail.com>
2021-02-16 10:47:26 -0500

    data length assertion

--- test/channels.ts
+++ test/channels.ts
@@ -8,9 +8,11 @@ const data = [137, 42, 123, 86, 34, 72, 21] as StaticArray<u8>;
 let c = Channel.create();
 // send some data
 c.send(data);
-// runtime assertion that it comes back
+// runtime assertion that the reference comes back
 let result  = c.receive()!;
 
+// assert the length and values of the data is correct
+assert(data.length == result.length);
 for (let i = 0; i < data.length; i++) {
     assert(data[i] == result[i]);
 }
