f3cf151a1987168976438722bb221b8615df690a
Joshua Tenner <tenner.joshua@gmail.com>
2021-02-16 13:04:09 -0500

    playground-fixes

--- assembly/index.d.ts
+++ assembly/index.d.ts
@@ -1,33 +1,30 @@
 declare module "lunatic" {
   // put lunatic types here
-  /** The channel namespace, used for creating communication channels between threads. */
-  export namespace Channel {
+  /** The channel namespace and Class, used for creating communication channels between threads. */
+  export class Channel {
     /**
      * A method for creating a brand new message channel with a message
      * count limit equal to the bound.
      *
      * @param {usize} bound - Default is [0], which provides an unbounded Message channel.
      */
-    export function create(bound?: usize): MessageChannel;
+    public static create(bound?: usize): Channel;
 
-    /** The MessageChannel class, used for communicating with a single channel between multiple threads. */
-    export class MessageChannel {
-      /** Deserialize a channel by it's serialized value, and obtain a reference to message and receive data through it. */
-      public static deserialize(value: u64): MessageChannel;
+    /** Deserialize a channel by it's serialized value, and obtain a reference to message and receive data through it. */
+    public static deserialize(value: u64): Channel;
 
-      /** The channel id for the sender portion of this MessageChannel. */
-      public sender: u32;
-      /** The channel id for the receiver portion of this MessageChannel. */
-      public receiver: u32;
+    /** The channel id for the sender portion of this MessageChannel. */
+    public sender: u32;
+    /** The channel id for the receiver portion of this MessageChannel. */
+    public receiver: u32;
 
-      /** Return a serialized value of this message channel. */
-      public serialize(): u64;
+    /** Return a serialized value of this message channel. */
+    public serialize(): u64;
 
-      /** A method for sending a data payload to a given MessageChannel object. */
-      public send(bytes: StaticArray<u8>): bool;
+    /** A method for sending a data payload to a given MessageChannel object. */
+    public send(bytes: StaticArray<u8>): bool;
 
-      /** A method for receiving a payload from a channel, returns null when there are no messages to recieve. */
-      public receive(): StaticArray<u8> | null;
-    }
+    /** A method for receiving a payload from a channel, returns null when there are no messages to recieve. */
+    public receive(): StaticArray<u8> | null;
   }
 }
--- assembly/lunatic.ts
+++ assembly/lunatic.ts
@@ -48,46 +48,44 @@ declare function receiver_deserialize(channel_id: u32): u32;
 const receive_length_pointer = memory.data(sizeof<u32>());
 
 
-// The channel namespace
-export namespace Channel {
 
-  // A message channel object
-  @final export class MessageChannel {
-    public sender: u32 = 0;
-    public receiver: u32 = 0;
 
-    // turns the u64 serialized ids into a MessageChannel for sending and receiving
-    public static deserialize(value: u64): MessageChannel {
-      let result = new MessageChannel();
-      result.sender = sender_deserialize(<u32>(u32.MAX_VALUE & value));
-      result.receiver = receiver_deserialize(<u32>(u32.MAX_VALUE & (value >>> 32)));
-      return result;
-    }
-
-    // the sender_serialize and receiver_serialize methods are host methods, used to encode channel ids
-    public serialize(): u64 {
-      return (<u64>sender_serialize(this.sender)) | (<u64>receiver_serialize(this.receiver) << 32);
-    }
-
-    // send some data
-    public send(bytes: StaticArray<u8>): bool {
-      return channel_send(this.sender, bytes, bytes.length) == ChannelSendResult.Success;
-    }
-
-    public receive(): StaticArray<u8> | null {
-      let prepareResult = channel_receive_prepare(this.receiver, receive_length_pointer);
-      let length = load<u32>(receive_length_pointer);
-      if (prepareResult == ChannelReceivePrepareResult.Fail) return null;
-      let result = new StaticArray<u8>(length);
-      channel_receive(result, length);
-      return result;
-    }
-  }
+// A message channel object
+@final export class Channel {
+  public sender: u32 = 0;
+  public receiver: u32 = 0;
 
   // create a brand new message channel
-  export function create(bound: usize = 0): MessageChannel {
-    let result = new MessageChannel();
-    result.sender = channel(bound, changetype<usize>(result) + offsetof<MessageChannel>("receiver"));
+  public static create(bound: usize = 0): Channel {
+    let result = new Channel();
+    result.sender = channel(bound, changetype<usize>(result) + offsetof<Channel>("receiver"));
+    return result;
+  }
+
+  // turns the u64 serialized ids into a Channel for sending and receiving
+  public static deserialize(value: u64): Channel {
+    let result = new Channel();
+    result.sender = sender_deserialize(<u32>(u32.MAX_VALUE & value));
+    result.receiver = receiver_deserialize(<u32>(u32.MAX_VALUE & (value >>> 32)));
+    return result;
+  }
+
+  // the sender_serialize and receiver_serialize methods are host methods, used to encode channel ids
+  public serialize(): u64 {
+    return (<u64>sender_serialize(this.sender)) | (<u64>receiver_serialize(this.receiver) << 32);
+  }
+
+  // send some data
+  public send(bytes: StaticArray<u8>): bool {
+    return channel_send(this.sender, bytes, bytes.length) == ChannelSendResult.Success;
+  }
+
+  public receive(): StaticArray<u8> | null {
+    let prepareResult = channel_receive_prepare(this.receiver, receive_length_pointer);
+    let length = load<u32>(receive_length_pointer);
+    if (prepareResult == ChannelReceivePrepareResult.Fail) return null;
+    let result = new StaticArray<u8>(length);
+    channel_receive(result, length);
     return result;
   }
 }
