1a31579171d2b94233f74778e4776f9fd094b828
Joshua Tenner <tenner.joshua@gmail.com>
2021-02-17 09:37:26 -0500

    oops, save changes

--- test/process.ts
+++ test/process.ts
@@ -2,7 +2,6 @@ import "wasi";
 import { Process, Channel } from "lunatic";
 import { Console } from "as-wasi";
 
-<<<<<<< HEAD
 let numbers = Channel.create(0);
 numbers.send([0, 1, 2]);
 
@@ -15,20 +14,6 @@ let p = Process.spawn<u64>(numbers.serialize(), (val: u64) => {
   assert(a[2] == 2);
   numbers.send([42]);
 });
-=======
-// @ts-ignore
-@external("lunatic", "spawn_with_context")
-declare function spawn_with_context(callback: u32, ptr: usize, size: usize): u32;
-// @ts-ignore
-@external("lunatic", "join")
-declare function join(pid: u32): u32;
-
-let closure = (): void => {
-  Console.log("Hello thread!\r\n");
-};
-let table_index = load<u32>(<u32>changetype<usize>(closure));
-let pid = spawn_with_context(table_index, 0, 0);
->>>>>>> 74ba5596157cf886737373b4513ba474fef71219
 
 // assert(p.join());
 p.join();
