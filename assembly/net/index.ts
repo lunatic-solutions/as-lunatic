38cd6926fae8cb7bf4d914b16da6bc4a6e42c670
Josh Tenner <jtenner@benecare.com>
2021-08-05 15:46:25 -0400

    More

--- assembly/net/index.ts
+++ assembly/net/index.ts
@@ -70,7 +70,7 @@ export class TCPResult<T> {
 /**
  * A resulting IPResolution.
  */
-export class IPResolution {
+export class IPAddress {
   /** An array of bytes representing the IP address. */
   address: StaticArray<u8> | null = null;
   /** The length of the IP address. 4 or 16. */
@@ -81,6 +81,17 @@ export class IPResolution {
   flow_info: u32;
   /** The scope_id. */
   scope_id: u32;
+
+  /** Clone an IP. */
+  clone(): IPAddress {
+    let result = new IPAddress();
+    result.address = this.address ? this.address.slice(0) : null;
+    result.address_len = this.address_len;
+    result.port = this.port;
+    result.flow_info = this.flow_info
+    result.scope_id = this.scope_id;
+    return result;
+  }
 }
 
 /**
@@ -92,6 +103,28 @@ export class IPResolution {
 @external("lunatic", "drop_tcp_listener")
 declare function drop_tcp_listener(listener: u64): void;
 
+/** Drop a tcp stream by it's id. */
+// @ts-ignore: valid decorator
+@external("lunatic", "drop_tcp_stream")
+declare function drop_tcp_stream(stream: u64): void;
+
+/** Clone a socket, return it's new id. */
+// @ts-ignore: valid decorator
+@external("lunatic", "clone_tcp_stream")
+declare function clone_tcp_stream(stream: u64): u64;
+
+/**
+ * Block the current process and read from the given TCPStream.
+ *
+ * @param {u64} stream - The given stream id.
+ * @param {usize} ptr - [*mut u8] A pointer to an allocated space of memory for the bytes.
+ * @param {usize} len - The maximum number of bytes to be read.
+ * @param {usize} opaque - [*mut usize] A pointer to write the number of bytes written.
+ */
+// @ts-ignore: valid decorator
+@external("lunatic", "tcp_read")
+declare function tcp_read(stream: u64, ptr: usize, len: usize, opaque: usize): err_code;
+
 /** A pointer to an ip resolution iterator. */
 const resolverIdPtr = memory.data(sizeof<u64>());
 
@@ -99,9 +132,9 @@ const resolverIdPtr = memory.data(sizeof<u64>());
  * Resolve an ip address from a host name.
  *
  * @param {string} host - The host or ip address name that should be resolved.
- * @returns {IPResolution[] | null} null if the IP could not be resolved.
+ * @returns {IPAddress[] | null} null if the IP could not be resolved.
  */
-export function resolve(host: string): TCPResult<IPResolution[] | null> {
+export function resolve(host: string): TCPResult<IPAddress[] | null> {
   // encode the ip address to utf8
   let ipBuffer = String.UTF8.encode(host);
   // call the host to resolve the IP address
@@ -116,9 +149,9 @@ export function resolve(host: string): TCPResult<IPResolution[] | null> {
     // read the resolver id
     let resolverId = load<u64>(resolverIdPtr);
 
-    return new TCPResult<IPResolution[] | null>(null, dns_iterator_to_array(resolverId));
+    return new TCPResult<IPAddress[] | null>(null, dns_iterator_to_array(resolverId));
   } else {
-    return new TCPResult<IPResolution[] | null>(
+    return new TCPResult<IPAddress[] | null>(
       getError(load<u64>(resolverIdPtr)),
       null,
     );
@@ -186,7 +219,7 @@ declare function tcp_connect(
   listener_id: usize, // *mut u64,
 ): err_code;
 
-const tcpStreamIdPtr = memory.data(sizeof<u64>());
+const opaqueu64Ptr = memory.data(sizeof<u64>());
 
 /**
  * This class represents a TCPServer. In order to obtain a reference to a TCPServer,
@@ -248,8 +281,8 @@ const tcpStreamIdPtr = memory.data(sizeof<u64>());
    * @returns {TCPStream | null} null if the tcp server errored.
    */
   public accept(): TCPResult<TCPStream | null> {
-    let code = tcp_accept(this.listener, tcpStreamIdPtr, resolverIdPtr);
-    let tcpStreamId = load<u64>(tcpStreamIdPtr);
+    let code = tcp_accept(this.listener, opaqueu64Ptr, resolverIdPtr);
+    let tcpStreamId = load<u64>(opaqueu64Ptr);
 
     if (code == err_code.Success) {
       let resolution = dns_iterator_to_array(load<u64>(resolverIdPtr));
@@ -279,23 +312,23 @@ const tcpStreamIdPtr = memory.data(sizeof<u64>());
  * @param {u64} id - The iterator.
  * @returns an array of IPResolution.
  */
-export function dns_iterator_to_array(id: u64): IPResolution[] {
+export function dns_iterator_to_array(id: u64): IPAddress[] {
   // loop over each IPResolution and add it to the list
-  let ipArray = new Array<IPResolution>(0);
+  let ipArray = new Array<IPAddress>(0);
   while (true) {
     // must always allocate 16 bytes
     let buffer = new StaticArray<u8>(16);
-    let resolution = new IPResolution();
+    let resolution = new IPAddress();
     resolution.address = buffer;
 
     // the host iterates over each result until it returns Done
     let resolutionResult = resolve_next(
       id,
       changetype<usize>(buffer),
-      changetype<usize>(resolution) + offsetof<IPResolution>("addr_len"),
-      changetype<usize>(resolution) + offsetof<IPResolution>("port"),
-      changetype<usize>(resolution) + offsetof<IPResolution>("flow_info"),
-      changetype<usize>(resolution) + offsetof<IPResolution>("scope_id"),
+      changetype<usize>(resolution) + offsetof<IPAddress>("addr_len"),
+      changetype<usize>(resolution) + offsetof<IPAddress>("port"),
+      changetype<usize>(resolution) + offsetof<IPAddress>("flow_info"),
+      changetype<usize>(resolution) + offsetof<IPAddress>("scope_id"),
     );
     if (resolutionResult == ResolveNextResult.Done) break;
     ipArray.push(resolution);
@@ -304,7 +337,7 @@ export function dns_iterator_to_array(id: u64): IPResolution[] {
   return ipArray;
 }
 
-export function connect_ip(ip: IPResolution): TCPResult<TCPStream | null> {
+export function connect_ip(ip: IPAddress): TCPResult<TCPStream | null> {
   return connect_unsafe(
     changetype<usize>(ip.address),
     ip.address_len,
@@ -329,6 +362,17 @@ export function connect(
   );
 }
 
+/**
+ * Actually requests a host connection with a raw pointer to address data. Considdered unsafe
+ * because passing around pointers can be dangerous.
+ *
+ * @param {usize} address_ptr - [u8*] The address pointer, 4 or 16 bytes in length
+ * @param {usize} address_len - The address length, 4 or 16
+ * @param {u16} port - The port number for the connection.
+ * @param {u32} flow_info - Flow info, [ipv6]
+ * @param {u32} scope_id - Scope id, [ipv6]
+ * @returns {TCPResult<TCPStream | null>} The result of attempting to open a tcp connection with a server.
+ */
 // @ts-ignore: valid decorator
 @unsafe export function connect_unsafe(
   address_ptr: usize,
@@ -343,11 +387,11 @@ export function connect(
     port,
     flow_info,
     scope_id,
-    tcpStreamIdPtr,
+    opaqueu64Ptr,
   );
 
   if (result === err_code.Success) {
-    let resolution = new IPResolution();
+    let resolution = new IPAddress();
     let ip = new StaticArray<u8>(<i32>address_len);
     memory.copy(changetype<usize>(ip), address_ptr, address_len);
     resolution.address = ip;
@@ -358,19 +402,103 @@ export function connect(
     return new TCPResult<TCPStream | null>(
       null,
       new TCPStream(
-        load<u64>(tcpStreamIdPtr),
+        load<u64>(opaqueu64Ptr),
         resolution,
       ),
     );
   } else {
     return new TCPResult<TCPStream | null>(
-      getError(load<u64>(tcpStreamIdPtr)),
+      getError(load<u64>(opaqueu64Ptr)),
       null,
     );
   }
 }
 
 export class TCPStream {
-  constructor(public listener: u64, public ip: IPResolution) {}
+  private dropped: bool = false;
+
+  constructor(public listener: u64, public ip: IPAddress) {}
+
+  drop(): void {
+    drop_tcp_stream(this.listener);
+    this.dropped = true;
+  }
+
+  clone(): TCPResult<TCPStream | null> {
+    if (this.dropped) {
+      return new TCPResult<TCPStream | null>(
+        "Cannot clone a dropped socket.",
+        null,
+      );
+    }
+    return new TCPResult<TCPStream | null>(
+      null,
+      new TCPStream(
+        clone_tcp_stream(this.listener),
+        this.ip.clone(),
+      ),
+    );
+  }
+
+  /**
+   * Read data from this tcp stream into a given static array.
+   *
+   * @param {StaticArray<u8>} buffer - The buffer the bytes will be written to.
+   * @returns The number of bytes written.
+   */
+  read_static_array(buffer: StaticArray<u8>): TCPResult<usize> {
+    return this.read_unsafe(
+      changetype<usize>(buffer),
+      <usize>buffer.length,
+    );
+  }
+
+  /**
+   * Read data into a given typed array, will use the backing buffer's byteLength to
+   * determine the maximum number of bytes to be written.
+   *
+   * @param {U} buffer - The TypedArray that will be written to.
+   * @returns The number of bytes written. Can calculate the number of indicies written
+   * by using `<i32>(bytesWritten >>> alignof<valueof<U>>())`.
+   */
+  read_typed_array<U extends ArrayBufferView>(buffer: U): TCPResult<usize> {
+    let ptr = changetype<usize>(buffer.buffer) + <usize>buffer.byteOffset;
+    return this.read_unsafe(ptr, <usize>buffer.byteLength);
+  }
+
+  /**
+   * Read data from this tcp stream into a given array buffer.
+   *
+   * @param {ArrayBuffer} buffer - The buffer the bytes will be written to.
+   * @returns The number of bytes written.
+   */
+  read_array_buffer(buffer: ArrayBuffer): TCPResult<usize> {
+    return this.read_unsafe(
+      changetype<usize>(buffer),
+      <usize>buffer.byteLength,
+    );
+  }
+
+  /**
+   * Read data from this TCPStream, and return the number of bytes writen.
+   *
+   * @param {usize} ptr - [*mut u8] A pointer to the location where the bytes will be written.
+   * @param {usize} len - The maximum number of bytes to be written.
+   * @returns The number of bytes written.
+   */
+  read_unsafe(ptr: usize, len: usize): TCPResult<usize> {
+    let result = tcp_read(this.listener, ptr, len, opaqueu64Ptr);
+    if (result == err_code.Success) {
+      return new TCPResult<usize>(
+        null,
+        <usize>load<u64>(opaqueu64Ptr),
+      );
+    } else {
+      return new TCPResult<usize>(
+        getError(load<u64>(opaqueu64Ptr)),
+        0,
+      );
+    }
+  }
 }
 
