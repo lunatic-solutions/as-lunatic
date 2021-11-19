ccd93719529e422022d6d5ff2ec99c34b330ffec
Josh Tenner <jtenner@benecare.com>
2021-11-19 15:22:59 -0500

    call the bootstrap function

--- assembly/entry.ts
+++ assembly/entry.ts
@@ -1,3 +1,4 @@
-export function __lunatic_process_bootstrap(index: i32): void {
-  
+/** Required lunatic export to make processes start. */
+export function __lunatic_process_bootstrap(index: u32): void {
+  call_indirect(index);
 }
