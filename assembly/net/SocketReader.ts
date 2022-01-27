6a1617eea84177b84a7c4e86b181477fe6c17eb4
Josh Tenner <jtenner@benecare.com>
2022-01-27 13:29:52 -0500

    simplify socket reader pattern

--- assembly/net/SocketReader.ts
+++ assembly/net/SocketReader.ts
@@ -1,10 +1,14 @@
 import { NetworkResultType } from "../util";
 import { TCPSocket } from "../net";
 import { Process } from "../process";
-import { UnmanagedResult } from "../Error";
+import { Result } from "../Error";
 import { Mailbox } from "../messaging";
 
-export abstract class SocketListener {
+export abstract class SocketReader<T> {
+    constructor(
+        public socket: TCPSocket,
+        public parentProcess: Process<T> = Process.self<T>(),
+    ) {}
     public timeout: u32 = 0;
     public abstract onInitialize(): void;
     public abstract onRead(data: StaticArray<u8>): bool;
@@ -12,51 +16,35 @@ export abstract class SocketListener {
     public abstract onTimeout(): bool;
     public abstract onClose(): void;
     public abstract onFinalize(): void;
-}
 
-export class SocketReaderStart<TListener extends SocketListener> {
-    constructor(
-        public socket: TCPSocket,
-        public listener: TListener,
-    ) {}
-}
-
-export class SocketReader {
-    static createWith<TListener extends SocketListener>(socket: TCPSocket, listener: TListener): UnmanagedResult<SocketReader | null> {
-        let start = new SocketReaderStart<TListener>(socket, listener);
-        let result = Process.inheritSpawnWith<SocketReaderStart<TListener>, i32>(start,
-            (start: SocketReaderStart<TListener>, mb: Mailbox<i32>) => {
+    start(): Result<Process<i32> | null> {
+        let result = Process.inheritSpawnWith<SocketReader<SocketReader<T>>, i32>(this,
+            (start: SocketReader<SocketReader<T>>, mb: Mailbox<i32>) => {
                 let socket = start.socket;
-                let listener = start.listener;
-                listener.onInitialize();
+                start.onInitialize();
                 while(true) {
-                    let result = socket.read(listener.timeout);
+                    let result = socket.read(start.timeout);
                     if (result.isOk()) {
                         let readStatus = result.expect();
                         if (readStatus == NetworkResultType.Closed) {
-                            listener.onClose();
+                            start.onClose();
                             break;
                         } else if (readStatus == NetworkResultType.Timeout) {
-                            if (listener.onTimeout()) break;
+                            if (start.onTimeout()) break;
                             continue;
                         } else if (readStatus == NetworkResultType.Success) {
-                            if (listener.onRead(socket.buffer!)) break;
+                            if (start.onRead(socket.buffer!)) break;
                             continue;
                         }
                     } else {
-                        listener.onError(result.errorString);
+                        start.onError(result.errorString);
                         break;
                     }
                 }
-                listener.onFinalize();
+                start.onFinalize();
             },
         );
-
-        if (result.isOk()) return new UnmanagedResult<SocketReader | null>(new SocketReader(result.expect()));
-        return new UnmanagedResult<SocketReader | null>(null, result.errorString);
+        return result;
     }
-
-    constructor(
-        public process: Process<i32>,
-    ) {}
 }
+
