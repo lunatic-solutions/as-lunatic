dc5cae46b370ff342cd0b3ba842301ae01d24b10
Josh Tenner <jtenner@benecare.com>
2022-01-27 20:11:48 -0500

    Refactor EventListener into a single class

--- assembly/process/EventListener.ts
+++ assembly/process/EventListener.ts
@@ -1,23 +1,44 @@
 import { Mailbox, MessageType } from "..";
-import { UnmanagedResult } from "../error";
+import { Result } from "../error";
 import { Process } from "./index";
 
 export abstract class EventListener<TBaseEvent> {
+    /** How long to wait for messages before a timeout occurs. */
     timeout: u32 = 0;
+    /** A collection of tags to listen for. */
     tags: StaticArray<i64> | null = null;
+    /** The process listening for events. */
+    process: Process<TBaseEvent> | null = null;
+
+    /** A method called when the EventListener starts up. */
     abstract onInitialize(): void;
+    /** A method called when the EventListener listens for a message and the call times out. */
     abstract onTimeout(): bool;
+    /**
+     * A method called whenever an event occurs.
+     *
+     * @param {TBaseEvent} ev - The base class of the event being emitted.
+     * @returns {bool} True if the event listener should stop listening and close.
+     */
     abstract onEvent(ev: TBaseEvent): bool;
+    /**
+     * A method called whenever a signal is received that a process died.
+     *
+     * @param {u64} tag - The process tag that was received in the signal.
+     * @returns {bool} True if the event listener should stop listening and close.
+     */
     abstract onSignal(tag: u64): bool;
+    /** A method called whenever resources are finalized in the listening process. */
     abstract onFinalize(): void;
-}
 
-export class EventEmitter<TBaseEvent> {
-
-    static createWith<TBaseEvent, TEventListener extends EventListener<TBaseEvent>>(
-        context: TEventListener
-    ): UnmanagedResult<EventEmitter<TBaseEvent> | null> {
-        let result = Process.inheritSpawnWith<TEventListener, TBaseEvent>(context, (ctx: TEventListener, mb: Mailbox<TBaseEvent>) => {
+    /**
+     * Start listening to events on a new process.
+     *
+     * @returns {Result<Process<TBaseEvent> | null>} The result of creating the process the event listener is listening on.
+     */
+    start(): Result<Process<TBaseEvent> | null>  {
+        assert(!this.process);
+        let result = Process.inheritSpawnWith<EventListener<TBaseEvent>, TBaseEvent>(this, (ctx: EventListener<TBaseEvent>, mb: Mailbox<TBaseEvent>) => {
             ctx.onInitialize();
 
             while (true) {
@@ -34,16 +55,12 @@ export class EventEmitter<TBaseEvent> {
 
             ctx.onFinalize();
         });
-
-        if (result.isOk()) return new UnmanagedResult<EventEmitter<TBaseEvent> | null>(new EventEmitter<TBaseEvent>(result.value!));
-        return new UnmanagedResult<EventEmitter<TBaseEvent> | null>(null, result.errorString);
+        if (result.isOk()) this.process = result.expect();
+        return result;
     }
 
-    constructor(
-        public process: Process<TBaseEvent>,
-    ) {}
-
+    /** Emit an event to the listening process. */
     emit<EventType extends TBaseEvent>(event: EventType): void {
-        this.process.send<EventType>(event);
+        this.process!.send<EventType>(event);
     }
 }
