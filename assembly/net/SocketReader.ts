7be7f03c6456eb7929c814dd20c6d8e5045ca703
Joshua Tenner <tenner.joshua@gmail.com>
2022-01-28 11:55:00 -0500

    lots of comments on socket readers

--- assembly/net/SocketReader.ts
+++ assembly/net/SocketReader.ts
@@ -4,22 +4,32 @@ import { Process } from "../process";
 import { Result } from "../Error";
 import { Mailbox } from "../messaging";
 
-export abstract class SocketReader<T> {
+/** Represents the base class for a SocketReader, which should have a generic type the same as the mailbox type of the parent process attempting to read from it. */
+export abstract class SocketReader<TParentEventBase> {
     constructor(
+        /** This socket is the socket that will be read from. */
         public socket: TCPSocket,
-        public parentProcess: Process<T> = Process.self<T>(),
+        /** This process is normally the current process, and the generic type of the process is usually the mailbox event type. */
+        public parentProcess: Process<TParentEventBase> = Process.self<TParentEventBase>(),
     ) {}
     public timeout: u32 = 0;
+    /** This method is called when the child process initializes. */
     public abstract onInitialize(): void;
+    /** This method is called when data is read from the socket. */
     public abstract onRead(data: StaticArray<u8>): bool;
+    /** This method is called when an error occurs while reading from a socket. It should return `true` if the socket should stop reading. */
     public abstract onError(desc: string): void;
+    /** This method is called when a socket read times out. It should return `true` if the socket should stop reading. */
     public abstract onTimeout(): bool;
+    /** This method is called when the socket is closed. */
     public abstract onClose(): void;
+    /** This method is called if the socket closed or the socket stops reading, and no errors caused a web assembly trap. */
     public abstract onFinalize(): void;
 
+    /** Start reading from the socket on another child process. */
     start(): Result<Process<i32> | null> {
-        let result = Process.inheritSpawnWith<SocketReader<SocketReader<T>>, i32>(this,
-            (start: SocketReader<SocketReader<T>>, mb: Mailbox<i32>) => {
+        let result = Process.inheritSpawnWith<SocketReader<SocketReader<TParentEventBase>>, i32>(this,
+            (start: SocketReader<SocketReader<TParentEventBase>>, mb: Mailbox<i32>) => {
                 let socket = start.socket;
                 start.onInitialize();
                 while(true) {
@@ -45,6 +55,10 @@ export abstract class SocketReader<T> {
             },
         );
         return result;
+    }
+    /** Send a message of type TEvent to the parent process. */
+    send<TEvent extends TParentEventBase>(message: TEvent, tag: u64 = 0): void {
+        this.parentProcess.send<TEvent>(message, tag);
     }
 }
 
